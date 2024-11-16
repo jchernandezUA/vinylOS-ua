c63494c11227711b632ae58098dfc67e5f534176
Jerson Florez <ja.florezr1@uniandes.edu.co>
2024-11-16 14:42:43 -0500

    Finalizar pantalla del detalle de un album

--- app/src/main/java/com/uniandes/vynilos/presentation/ui/screen/album/AlbumListScreen.kt
+++ app/src/main/java/com/uniandes/vynilos/presentation/ui/screen/album/AlbumListScreen.kt
@@ -33,11 +33,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -209,7 +207,6 @@ fun AlbumsCard(
             )
         }
     }
-
 }
 
 @PreviewLightDark
